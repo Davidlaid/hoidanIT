5b5c96ab81ad38cae18af3904ae8205e83c9dda2
tuyen202 <nguyendangtuyen352@gmail.com>
2022-09-15 01:21:39 +0700

    get data form input

--- src/views/Exemple/MyComponent.js
+++ src/views/Exemple/MyComponent.js
@@ -5,15 +5,48 @@ class MyComponent extends React.Component {
     firstName: '',
     lastName: ''
   }
+
+  handleChangeFirstName = (event) => {
+    this.setState({
+      firstName: event.target.value
+    })
+  }
+
+  handleChangeLastName = (event) => {
+    this.setState({
+      lastName: event.target.value
+    })
+  }
+
+  // get data
+  handlClickGetApi = () => {
+    console.log(this.state);
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div>
         <form action="/action_page.php">
-        <label for="fname">First name:</label><br/>
-        <input type="text" value={this.state.firstName}/><br/>
-        <label for="lname">Last name:</label><br/>
-        <input type="text"  value=""/><br/><br/>
-        <input type="submit" value="Submit"/>
+        <label htmlFor="fname">First name:</label><br/>
+        <input 
+          type="text" 
+          value={this.state.firstName}
+          onChange={(event)=> {this.handleChangeFirstName(event)}}
+        />
+        <br/>
+        <label htmlFor="lname">Last name:</label><br/>
+        <input 
+          type="text"
+          value={this.state.lastName}
+          onChange={(event)=>{this.handleChangeLastName(event)}}
+        />
+        <br/><br/>
+        <input 
+          type="button" 
+          value="click me"
+          onClick={(event)=>{this.handlClickGetApi(event)}}
+        />
       </form> 
       </div>
     )
@@ -22,6 +55,25 @@ class MyComponent extends React.Component {
 
 export default  MyComponent;
 
-//? BT:  Thu Thập Dữ Liệu Inputs HTML
+//! BT:  Thu Thập Dữ Liệu Inputs HTML
 // Bước 1: add html, jsx
-// Bước 2: tạo state
+// Bước 2: 
+/*
+- tạo state
+- gán this.state.value vafo input tag theo biến state
+*/
+
+//? Tới bước này ta vẫn chưa nhập được dữ liệu do ta thiếu handle event
+// Bước 3:  thêm event onChange vào input tag
+//? Lý do thêm event onChange vì đây là tag input yêu cầu người dùng nhập dữ liệu,
+//?hàm onChange xử lý sự thay đổi khi user nhập dữ liệu
+/*
+- create arrow function named handleChangeFirstName để gán vào hàm onChange
+- thêm hàm onChange vào input tag rồi gán hàm handleChangeFirstName  (fucntion react)
+*/
+
+// Bước 4: lấy dữ liệu
+/*
+- tạo hàm handlClickGetApi
+- thêm hàm onClick vào button rồi gán hàm handlClickGetApi
+*/
